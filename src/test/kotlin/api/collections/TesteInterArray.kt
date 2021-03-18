8891d283b2db9e7388e9fe2fe7fe5cd3a11ebee0
Predo <josepedroxa.design@gmail.com>
2021-03-18 11:05:00 -0300

    lesson ArrayOF

---
+++ src/test/kotlin/api/collections/TesteInterArray.kt
@@ -0,0 +1,26 @@
+package api.collections
+
+fun main() {
+    val values = IntArray(size = 5);
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        println(valor)
+    }
+    println("------------------")
+    values.forEach { valor ->
+        println(valor)
+    }
+    for (index in values.indices) {
+        println(values[index])
+    }
+    println("------------------")
+    values.sort()
+    for (valor in values) {
+        println(values[valor])
+    }
+}
